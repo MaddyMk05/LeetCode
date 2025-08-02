2015f5992a6c9f11f04bace408e0ce41b09d2682
MaddyMk05 <maadhava05@gmail.com>
2025-08-02 10:29:04 +0530

    Time: 15 ms (20.54%) | Memory: 45.3 MB (19.70%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -3,15 +3,8 @@ class Solution {
         if(s==" "){
             return true;
         }
-        String str=s.toLowerCase();
-        char [] ch=str.toCharArray();
-        String t1 = "";
-        for(int i=0;i<str.length();i++){
-            if((ch[i]>='a' && ch[i]<='z')||(ch[i] >= '0' && ch[i] <= '9')){
-                t1 += ch[i];
-            }
-        }
-        String t2 = new StringBuilder(t1).reverse().toString();
-        return t1.equals(t2);
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        String t2 = new StringBuilder(s).reverse().toString();
+        return s.equals(t2);
     }
 }
